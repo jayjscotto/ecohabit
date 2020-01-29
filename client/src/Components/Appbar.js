1a9b6d6761c9988ca6f4af7dc7170bd62efef4be
Jason <jayjscotto@gmail.com>
2020-01-29 15:44:59 -0500

    updating with userquery branch code

--- client/src/Components/Appbar.js
+++ client/src/Components/Appbar.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton
-} from '@material-ui/core';
-=======
-import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button, IconButton, Link,  } from '@material-ui/core';
-import Daily from '../Pages/Daily';
->>>>>>> 5eae9381685aacd8c98c80b41d67e5c03ccdf6a3
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
 // import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
