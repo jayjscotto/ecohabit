3d419c949733e4ee6f0ce406279f043ee345eda2
limabean <lditom808@gmail.com>
2020-02-01 11:55:17 -0500

    spelling fixes

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const bcrypt = require('bcrypt-nodejs');
 const UserSchema = new Schema({
 	userName: {
 		type: String,
-		required: true
+		required: true,
+		unique: true
 	},
 	password: {
 		type: String,
