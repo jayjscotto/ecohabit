dc2714b41649f4cc73aec950cdd5108ac06862f2
Matt Pignatore <mattpigs@gmail.com>
2020-02-03 20:14:33 -0500

    npm installed chart.js and got the basic line chart to render in the daily panel

--- client/src/Components/RightPane.js
+++ client/src/Components/RightPane.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Grid, Paper, Typography } from '@material-ui/core';
+import { Bar, Line } from 'react-chartjs-2';
+
+let data = {
+    data: {
+        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June'],
+        datasets: [{
+            label: 'Ecohabits',
+            data: [0, 3, 2, 2, 1, 4, 3, 1, 2, 5, 4, 2],
+            backgroundColor: [
+                'rgba(241, 187, 135, 0.2)'
+            ],
+            borderColor: [
+                'rgba(93, 103, 91, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+}
 
 function RightPane(props) {
 
@@ -10,6 +28,12 @@ function RightPane(props) {
                     <Typography style={props.header}>
                         Daily Dashboard
                     </Typography>
+                    <Line
+                    data={data.data}
+                    width={100}
+                    height={40}
+                    options={{ maintainAspectRatio: true }}
+                    />
                 </Paper>
             </Grid>
             <Grid item sm={12}>
