72fb58a40ba19df3e9764d2c3fa86414bec970dc
limabean <lditom808@gmail.com>
2020-02-07 11:53:57 -0500

    change to styling of about page

--- client/src/Components/landingPage/landingAbout.js
+++ client/src/Components/landingPage/landingAbout.js
@@ -17,49 +17,50 @@ class About extends Component {
 	};
 	render() {
 		return (
-			<Grid
-				container
-				justify="center"
+			<div
 				style={{
+					height: '90vh',
 					background: `url(${BackgroundGif}) no-repeat`,
 					backgroundSize: 'cover'
 				}}
 			>
-				<Grid item sm={12} style={{ padding: '150px 20px 0px 20px' }}>
-					<Grid item sm style={{ textAlign: 'center' }}>
-						<h1 style={{ color: 'rgb(210,209,205)' }}>
-							<em>EcoHabit is here to Help.</em>
-						</h1>
-					</Grid>
-					<Grid item sm />
-					<Grid item sm style={{ textAlign: 'center' }}>
-						<Container alignItems="center" maxWidth="sm">
-							<p style={{ lineHeight: '1.2', textAlign: 'center', color: 'rgb(210,209,205)' }}>
-								<em>
-									EcoHabit is a daily diary mean to keep it's users on track to becoming a better eco
-									Citizen. Designed with simplicity in mind, EcoHabit offers users an interactive and
-									inuitive experience.
-									<br />
-									EcoHabit is dedicated helping our users improve themselves. We're here daily to
-									remind you that you are important and you choice do matter. No matter how small or
-									how alien you may ever feel, we're here to remind you that <em>this</em> is home and{' '}
-									<em>we</em> are all going to work together to take care of it.
-								</em>
-							</p>
-						</Container>
-					</Grid>
-					<Grid item sm={12}>
-						<div style={{ display: 'flex', justifyContent: 'center', marginTop: '130px' }}>
-							<FormButton label="Back" primary={true} onClick={this.back}>
-								<ArrowBackIcon />
-							</FormButton>
-							<FormButton label="Continue" primary={false} onClick={this.continue}>
-								<ArrowForwardIcon />
-							</FormButton>
-						</div>
+				<Grid container justify="center">
+					<Grid item sm={12} style={{ padding: '150px 20px 0px 20px' }}>
+						<Grid item sm style={{ textAlign: 'center' }}>
+							<h1 style={{ color: 'rgb(210,209,205)' }}>
+								<em>EcoHabit is here to Help.</em>
+							</h1>
+						</Grid>
+						<Grid item sm />
+						<Grid item sm style={{ textAlign: 'center' }}>
+							<Container alignItems="center" maxWidth="sm">
+								<p style={{ lineHeight: '1.2', textAlign: 'center', color: 'rgb(210,209,205)' }}>
+									<em>
+										EcoHabit is a daily diary mean to keep it's users on track to becoming a better
+										eco Citizen. Designed with simplicity in mind, EcoHabit offers users an
+										interactive and inuitive experience.
+										<br />
+										EcoHabit is dedicated helping our users improve themselves. We're here daily to
+										remind you that you are important and you choice do matter. No matter how small
+										or how alien you may ever feel, we're here to remind you that <em>this</em> is
+										home and <em>we</em> are all going to work together to take care of it.
+									</em>
+								</p>
+							</Container>
+						</Grid>
+						<Grid item sm={12}>
+							<div style={{ display: 'flex', justifyContent: 'center', marginTop: '130px' }}>
+								<FormButton label="Back" primary={true} onClick={this.back}>
+									<ArrowBackIcon />
+								</FormButton>
+								<FormButton label="Continue" primary={false} onClick={this.continue}>
+									<ArrowForwardIcon />
+								</FormButton>
+							</div>
+						</Grid>
 					</Grid>
 				</Grid>
-			</Grid>
+			</div>
 		);
 	}
 }
