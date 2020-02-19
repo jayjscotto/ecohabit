6a4da8ca8023a975185bcfd31d60ba27c6357466
Matt Pignatore <mattpigs@gmail.com>
2020-02-19 12:45:41 -0500

    adding the gif to the readme

--- client/src/Components/DataDisplay.js
+++ client/src/Components/DataDisplay.js
@@ -2,6 +2,17 @@ import React, {useState, Fragment} from 'react';
 import { Grid, Paper} from '@material-ui/core';
 // import API from '../Utils/electric-api';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+})
 
 let useStyles = makeStyles({
   root: {
@@ -30,9 +41,11 @@ let useStyles = makeStyles({
 function DataDisplay(props) {
   let classes = useStyles();
   const [modalOpen, setModalOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
 
   const handleClickOpen = () => {
     console.log('open!')
+    setLoading(false);
     setModalOpen(true);
   }
 
@@ -61,7 +74,28 @@ function DataDisplay(props) {
             </li>
           </ul>
         </Paper>
+        <Dialog
+          open={modalOpen}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>Map Modal</DialogTitle>
+          <DialogContent>
+            {loading
+            ?
+            null
+            :
+            <iframe src={`https://www.google.com/maps/place/${googleLink}`} width="400" />
+            }
+            
+          </DialogContent>
+          <DialogActions></DialogActions>
+        </Dialog>
         </Grid>
+
       )
   })
 
